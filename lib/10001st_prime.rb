c907f85e0974dbf345c8527a74e62f68cc2e5dd6
dwandrew <danwandrew@gmail.com>
2020-11-16 14:58:11 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,5 @@
 # Implement your procedural solution here!
+require 'Prime'
+def prime_number_for(nth_element)
+    Prime.take(nth_element).last
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,16 @@
 # Implement your object-oriented solution here!
+require 'Prime'
+
+
+class PrimeNum
+attr_accessor :num
+
+def initialize(num)
+    @num = num
+end
+
+def number
+    Prime.take(self.num).last
+end
+
+end
